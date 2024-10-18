4f275608b265f1c52c514f8219b7d363355ca79e
Solisgarcia <126632227+Solisgarcia@users.noreply.github.com>
2024-10-18 14:23:38 -0600

    Update While.c

--- While.c
+++ While.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>  
+
+int main ()
+{
+   int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+   int i = 0, j;
+
+   printf("Imprimir matriz\n");
+
+   while(i < 3) 
+   {
+      j = 0; // Reiniciamos j para cada fila
+      while(j < 3) 
+      {
+         printf("%d ", arr[i][j]);
+         j++;
+      }
+      printf("\n");
+      i++;
+   }
+
+   return 0;
+}
